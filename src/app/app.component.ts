a8d9a8468d1090860ae4e56f894d3ceb7a48bb10
Dan Fearing <dan.fearing@gmail.com>
2017-02-01 21:14:32 -0800

    Trying to get Github Pages to work!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as d3 from "d3";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    private containerWidth: number;
+    private svg: any;
+    title = 'app works!';
+
+    @ViewChild("container")
+    container: ElementRef;
+
+    constructor() {
+
+    }
+
+    ngAfterViewInit(): void {
+        this.initializeSvg(500);
+    }
+
+    initializeSvg(height: number, width: number = null): void {
+        let bounds = this.container.nativeElement.getBoundingClientRect();
+        this.containerWidth = width ? width : Math.floor(bounds.width);
+
+        this.svg = d3.select(this.container.nativeElement)
+            .append("svg")
+            .attr("width", this.containerWidth)
+            .attr("height", height)
+            .append("g");
+    }
 }
