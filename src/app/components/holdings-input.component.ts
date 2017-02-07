ffa3fe40d02619aa8a852a94a516287fd0aeeac2
Dan Fearing <dan.fearing@gmail.com>
2017-02-06 21:30:10 -0800

    Missed these!

---
+++ src/app/components/holdings-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Inject } from "@angular/core";
+import { Subject } from "rxjs/Rx";
+
+import { APP_SERVICE, AppService } from "../services/app.service";
+
+@Component({
+    selector: 'holdings-input',
+    templateUrl: './holdings-input.component.html'
+})
+export class HoldingsInputComponent {
+    private onChange: Subject<number> = new Subject();
+
+    constructor(@Inject(APP_SERVICE)private appService: AppService) {
+        
+    }
+
+    ngAfterViewInit(): void {
+        this.onChange.debounceTime(500).subscribe(index => {
+            this.appService.calculateHoldingValue(index);
+        });
+    }
+}
